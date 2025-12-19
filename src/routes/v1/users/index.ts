f3be724ada7aa9c4ba9f9b9953ac8a6d3e04c102
zakariasamir <zakariasamir155@gmail.com>
2025-12-19 18:58:57 +0100

    Enable userId route: uncomment userId route in user routes file to allow fetching user-specific data, enhancing API functionality.

--- src/routes/v1/users/index.ts
+++ src/routes/v1/users/index.ts
@@ -1,17 +1,17 @@
 import { Router } from "express";
 import root from "./root";
-// import userId from "./[userId]";
+import userId from "./[userId]";
 
 const router = Router({ mergeParams: true });
 
 /**
- * api/v1/rooms
+ * api/v1/users
  */
 router.use("/", root);
 
 /**
- * api/v1/rooms/[roomId]
+ * api/v1/users/[userId]
  */
-// router.use("/:userId", userId);
+router.use("/:userId", userId);
 
 export default router;
