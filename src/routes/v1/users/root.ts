3789c1ca7d7e36a278aa1ae2c8b9e206cc783d1e
zakariasamir <zakariasamir155@gmail.com>
2025-12-19 18:59:19 +0100

    Refactor user retrieval: replace direct database query with service method to fetch user data, improving code organization and maintainability.

--- src/routes/v1/users/root.ts
+++ src/routes/v1/users/root.ts
@@ -8,7 +8,12 @@ const router = Router();
  */
 router.get("/", async (req: Request, res: Response): Promise<void> => {
   try {
-    const users = await User.find({});
+    const users = await User.services.fetchAll({
+      query: {
+        selection: ["username", "email", "profile_picture"],
+      },
+    });
+    res.status(200).json({ success: true, data: users });
   } catch (error) {
     console.error("Get all users error:", error);
     res.status(500).json({ message: "Server error" });
